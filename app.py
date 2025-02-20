5ab4e056d92bf94f1fb26694b41e6334cbc52789
Aymeric Dominique <aymeric.dominique@gmail.com>
2025-02-20 17:51:15 +0100

    tab

--- app.py
+++ app.py
@@ -1,54 +1,59 @@
 import streamlit as st
 import pandas as pd
-from utils import est_numero_declaration_veridique
+from utils import is_id_number_valid
+
+def analyze_data(source_name, source_file, teleservice_df, id_col, name_col, postal_code_col):
+    if source_file is not None:
+        st.header(f"Analyse {source_name}")
+        source_df = pd.read_csv(source_file, index_col=False)
+        source_df[id_col] = source_df[id_col].astype(str)
+
+        if st.button(f"Analyser {source_name}"):
+            merged_df = source_df.merge(
+                teleservice_df,
+                left_on=id_col,
+                right_on="numero_declaration",
+                suffixes=(f'_{source_name.lower()}', ''),
+                how="left"
+            )
+            st.dataframe(merged_df.head())
+
+            empty_id_mask = merged_df["numero_declaration"].isnull()
+            empty_id = empty_id_mask.sum()
+            st.write(f"Nombre de nuitées {source_name} ne correspondant pas à un id connu: {empty_id}")
+            with st.expander("Voir les nuitées sans id"):
+                empty_rows = merged_df[empty_id_mask][[id_col, name_col]]
+                st.dataframe(empty_rows)
+            
+            duplicate_id_mask = source_df[id_col].duplicated()
+            duplicate_id_source = duplicate_id_mask.sum()
+            st.write(f"Nombre d'id de nuitées en double: {duplicate_id_source}")
+            with st.expander("Voir les id en double"):
+                duplicate_rows = source_df[duplicate_id_mask][[id_col, name_col]]
+                st.dataframe(duplicate_rows)
+
+            invalid_id_mask = merged_df.apply(lambda x: is_id_number_valid(x[id_col], str(x[postal_code_col])), axis=1)
+            invalid_id_platform = invalid_id_mask.sum()
+            st.write(f"Nombre d'id de nuitées invalides: {invalid_id_platform}")
+            with st.expander("Voir les id invalides"):
+                invalid_rows = merged_df[~invalid_id_mask][[id_col, name_col]]
+                st.dataframe(invalid_rows)
 
 st.title("Analyse des données")
 
 with st.expander("Données"):
     teleservice_file = st.file_uploader("Fichier CSV téléservice", type="csv")
     airbnb_file = st.file_uploader("Fichier CSV Airbnb", type="csv")
-    # TODO Uncomment when it will be used
-    # booking_file = st.file_uploader("Fichier CSV Booking", type="csv")
+    booking_file = st.file_uploader("Fichier CSV Booking", type="csv")
 
-if teleservice_file is not None and airbnb_file is not None:
+if teleservice_file is not None:
     teleservice_df = pd.read_csv(teleservice_file, index_col=False)
     teleservice_df["numero_declaration"] = teleservice_df["numero_declaration"].astype(str)
+
+    tabs = st.tabs(["Airbnb", "Booking"])
+
+    with tabs[0]:
+        analyze_data("Airbnb", airbnb_file, teleservice_df, "Numéro de déclaration du meublé", "Nom du loueur", "Code postal")
     
-    airbnb_df = pd.read_csv(airbnb_file, index_col=False)
-    airbnb_df["Numéro de déclaration du meublé"] = airbnb_df["Numéro de déclaration du meublé"].astype(str)
-
-    if st.button("Analyser"):
-        # Merge the two DataFrames
-        airbnb_merged = airbnb_df.merge(
-          teleservice_df,
-          left_on="Numéro de déclaration du meublé",
-          right_on="numero_declaration",
-          suffixes=('_airbnb', ''),
-          how="left"
-        )
-        st.dataframe(airbnb_merged.head())
-
-        # count the number of empty values in id_internal
-        empty_id_mask = airbnb_merged["numero_declaration"].isnull()
-        empty_id = empty_id_mask.sum()
-        st.write(f"Nombre de nuitées ne correspondant pas à un id connu: {empty_id}")
-        with st.expander("Voir les nuitées sans id"):
-            empty_id = airbnb_merged[empty_id_mask]["Numéro de déclaration du meublé"]
-            st.write(empty_id)
-        
-        # count duplicates in id_platform
-        duplicates_id_mask = airbnb_df["Numéro de déclaration du meublé"].duplicated()
-        duplicates_id_airbnb = duplicates_id_mask.sum()
-        st.write(f"Nombre d'id de nuitées en double: {duplicates_id_airbnb}")
-        with st.expander("Voir les id en double"):
-            duplicates = airbnb_df[duplicates_id_mask]["Numéro de déclaration du meublé"]
-            st.write(duplicates)
-        
-        # count number of invalid id_platform
-        invalid_id_mask = airbnb_merged.apply(lambda x: est_numero_declaration_veridique(x["Numéro de déclaration du meublé"], str(x["Code postal"])), axis=1)
-        invalid_id_platform = invalid_id_mask.sum()
-        st.write(f"Nombre d'id de nuitées invalides: {invalid_id_platform}")
-        with st.expander("Voir les id invalides"):
-            invalid_id = airbnb_merged[~invalid_id_mask]["Numéro de déclaration du meublé"]
-            st.write(invalid_id)
-        
+    with tabs[1]:
+        analyze_data("Booking", booking_file, teleservice_df, "id_num", "nom_loueur", "ad_cp")
--- utils.py
+++ utils.py
@@ -1,25 +1,25 @@
       
-def est_numero_declaration_veridique(numero_declaration, code_postal):
+def is_id_number_valid(id_number, postal_code):
     """
     Vérifie si un numéro de déclaration de meublé de tourisme est vraisemblable
     pour un code postal donné à Paris.
 
     Args:
-        numero_declaration (str): Le numéro de déclaration à 13 chiffres.
-        code_postal (str): Le code postal de Paris (format "750XX").
+        id_number (str): Le numéro de déclaration à 13 chiffres.
+        postal_code (str): Le code postal de Paris (format "750XX").
 
     Returns:
         bool: True si le numéro de déclaration est vraisemblable pour le code postal, False sinon.
     """
-    if not isinstance(numero_declaration, str) or not numero_declaration.isdigit() or len(numero_declaration) != 13:
+    if not isinstance(id_number, str) or not id_number.isdigit() or len(id_number) != 13:
         return False  # Format du numéro de déclaration incorrect
 
-    if not isinstance(code_postal, str) or not code_postal.startswith("75") or len(code_postal) != 5 or not code_postal[2:].isdigit():
+    if not isinstance(postal_code, str) or not postal_code.startswith("75") or len(postal_code) != 5 or not postal_code[2:].isdigit():
         return False  # Format du code postal incorrect ou n'est pas un code postal parisien
 
-    code_insee_ville = numero_declaration[:2]
-    arrondissement_declaration_str = numero_declaration[2:5]
-    arrondissement_cp_str = code_postal[2:]
+    code_insee_ville = id_number[:2]
+    arrondissement_declaration_str = id_number[2:5]
+    arrondissement_cp_str = postal_code[2:]
 
     if code_insee_ville != "75":
         return False # Doit commencer par le code INSEE de Paris
